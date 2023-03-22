6f99a0f66f8b9f1ec39e50edad768328e81a1aae
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2023-03-22 21:35:13 +0300

    Add files via upload

---
+++ WeatherSwiftUI/WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Valentina Krysak on 22.03.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
