f565ace4a7c4a9671240a04af47298de6b12e9fe
Valentina <tinakrysak@gmail.com>
2023-03-23 21:08:33 +0300

    Tab Bar begin

--- WeatherSwiftUI/WeatherSwiftUI/Views/Main/HomeView.swift
+++ WeatherSwiftUI/WeatherSwiftUI/Views/Main/HomeView.swift
@@ -2,35 +2,59 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            // MARK: Background Color
-            Color.background
-                .ignoresSafeArea()
+        NavigationView {
+            ZStack {
+                // MARK: Background Color
+                Color.background
+                    .ignoresSafeArea()
 
-            // MARK: Background Image
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea()
+                // MARK: Background Image
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea()
 
-            // MARK: House Image
-            Image("House")
-                .frame(maxHeight: .infinity, alignment: .top)
-                .padding(.top, 257)
+                // MARK: House Image
+                Image("House")
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .padding(.top, 257)
 
-            VStack {
-                Text("Montreal")
-                    .font(.largeTitle)
+                VStack (spacing: -10){
+                    Text("Montreal")
+                        .font(.largeTitle)
 
-                VStack {
-                    Text("19°" + "\n" + "Mostly Clear")
-                    Text("H:24°   L:18°")
-                        .font(.title3.weight(.semibold))
+                    VStack {
+
+                        Text(attributedString)
+                            .foregroundColor(.secondary)
+                        Text("H:24°   L:18°")
+                            .font(.title3.weight(.semibold))
+                    }
+                    Spacer()
                 }
-                Spacer()
+                .padding(.top, 51)
+
+                TabBar(action: {})
             }
-            .padding(.top, 51)
+            .navigationBarHidden(true)
+        }
+    }
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n" + "Mostly Clear")
+
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
         }
 
+        return string
     }
 }
 
@@ -40,3 +64,4 @@ struct HomeView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
+
